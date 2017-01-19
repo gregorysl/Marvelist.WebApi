676bbddfe9d471625beda13e5931924179d04ebc
gregorysl <gregorysl@gmail.com>
2017-01-19 13:24:38 +0100

    Change Header

--- Marvelist.Web/src/components/common/Header.js
+++ Marvelist.Web/src/components/common/Header.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 
 const Header = (props) => {
-  return ( 
-    <div className="container">
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#">Marvelist</a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/books">Book</Link></li>
-              <li><Link to="/series">Series</Link></li>
-            </ul>
-          </div>
+  return (
+    <div className="navbar navbar-inverse navbar-fixed-top navbar-marvel">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target=".navbar-collapse">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a href="/">Marvelist</a>
         </div>
-      </nav>
+        <div className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/books">Book</Link>
+            </li>
+            <li>
+              <Link to="/series">Series</Link>
+            </li>
+            <li>
+              <label className="searchbox">
+                <input
+                  type="search"
+                  placeholder="Search"
+                  checked="checked"
+                  id="search-input"
+                  className="searchfield vcenter"/>
+              </label>
+            </li>
+          </ul>
+
+        </div>
+      </div>
     </div>
   );
 };
