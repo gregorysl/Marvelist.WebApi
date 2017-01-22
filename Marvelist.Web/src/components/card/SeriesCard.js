9372430ea428390d808826154737cda9b04b1c9a
gregorysl <gregorysl@gmail.com>
2017-01-22 16:16:25 +0100

    Change displayed series data to match backend

--- Marvelist.Web/src/components/card/SeriesCard.js
+++ Marvelist.Web/src/components/card/SeriesCard.js
@@ -9,27 +9,30 @@ class SeriesCard extends React.Component {
     render() {
         const url = require("file-loader!../../images/not_found.png");
         return (
-            <div className="grid-item col-sm-3 col-xs-4">
+            <div className="series-card col-sm-3 col-xs-4">
                 <div className="cover">
                     <img className="base" src={url}/>
-                    <img className="real" src={`${this.props.series.Series.Thumbnail.Path}.jpg`}/>
+                    <img className="real" src={`${this.props.series.Thumbnail}`}/>
                     <div className="shadow-base"></div>
                     <div className="titles">
                         <h3>
-                            <a href={`/Series/${this.props.series.Series.Id}`}>{this.props.series.Series.Title}</a>
+                            <a href={`/Series/${this.props.series.Id}`}>{this.props.series.Title}</a>
                         </h3>
                     </div>
                 </div>
                 <div className="quick-icons">
                     <div className="actions">
-                        <a className="watch">
-                            <div className="base"></div>
-                            <div className="fa fa-heart rating-8"></div>
+                        <a>
+                            <i className="fa fa-heart-o"></i>
+                        </a>
+                        <a>
+                            <i className="fa fa-external-link"></i>
                         </a>
                     </div>
                     <div className="metadata">
                         <div className="percentage" data-original-title="" title="">
-                            <div className="fa fa-heart rating-8"></div>81%</div>
+                            <i className="fa fa-folder-o"></i>{this.props.series.ComicCount}
+                        </div>
                     </div>
                 </div>
             </div>
