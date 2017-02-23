a1be12f9181a18142008c6acd72fc60c0a301b60
gregorysl <gregorysl@gmail.com>
2017-02-23 09:24:44 +0100

    Webpack cleanup and added antd

--- Marvelist.Web/src/index.js
+++ Marvelist.Web/src/index.js
@@ -1,18 +1,22 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import {Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+import 'antd/dist/antd.less';
+import './styles/main.less';
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
-  </Provider>,
+  <LocaleProvider locale={enUS}>
+    <Provider store={store}>
+      <Router history={browserHistory} routes={routes} />
+    </Provider>
+  </LocaleProvider>,
   document.getElementById('app')
 );
