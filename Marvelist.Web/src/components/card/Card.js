f4e5515b171d5445f0afe39eeaa14d2ef1e74e9d
gregorysl <gregorysl@gmail.com>
2017-02-26 10:45:55 +0100

    Change card to antd one

--- Marvelist.Web/src/components/card/Card.js
+++ Marvelist.Web/src/components/card/Card.js
@@ -2,10 +2,12 @@ import React, { PropTypes } from 'react';
 import Cover from './Cover';
 import ActionsBar from './ActionsBar';
 import CoverDate from './CoverDate';
+import { Spin, Row, Col, Card } from 'antd';
 
-class Card extends React.Component {
+class Card1 extends React.Component {
     constructor(props) {
         super(props);
+        this.loading = !!props.loading;
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
@@ -13,20 +15,25 @@ class Card extends React.Component {
     }
     render() {
         return (
-            <div className="series-card col-sm-3 col-xs-4">
-                <Cover link={this.props.link} {...this.props.data} />
-                {this.props.data.date && <CoverDate comicDate={this.props.data.date} />}
-                <ActionsBar click={this.handleClick} {...this.props.data} />
-            </div>
+
+            <Col className="series-card" xs={{ span: 8 }} sm={{ span: 6 }} lg={{ span: 4 }} >
+                <Spin size="large" spinning={this.loading}>
+                    <Card bodyStyle={{ padding: 0 }}>
+                        <Cover link={this.props.link} {...this.props.data} />
+                        {this.props.data.date && <CoverDate comicDate={this.props.data.date} />}
+                        <ActionsBar click={this.handleClick} {...this.props.data} />
+                    </Card>
+                </Spin>
+            </Col>
         );
     }
 }
 
-Card.propTypes = {
+Card1.propTypes = {
     toggleFollow: PropTypes.func,
     follow: PropTypes.func.isRequired,
     link: PropTypes.number,
     data: PropTypes.object.isRequired
 };
 
-export default Card;
+export default Card1;
--- Marvelist.Web/src/components/common/HomePage.js
+++ Marvelist.Web/src/components/common/HomePage.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Card from '../card/Card';
+import Card1 from '../card/Card';
 import { fetchHome, folllowComic } from '../../actions/seriesActions';
 import { PLACE } from '../../actions/constants';
 
@@ -22,7 +22,7 @@ class HomePage extends React.Component {
 
   render() {
     let {follow} = this.props;
-    const comics = this.hasData ? this.props.home.map((b, i) => <Card key={i} follow={follow} data={b} link={PLACE.HOME} />) : "";
+    const comics = this.hasData ? this.props.home.map((b, i) => <Card1 key={i} follow={follow} data={b} link={PLACE.HOME} loading={false} />) : "";
     return (
       <div className="row cards">
         {comics}
