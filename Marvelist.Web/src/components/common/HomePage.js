bb037e0688a51a7599bd0615a8b8d17bb5632bb1
gregorysl <gregorysl@gmail.com>
2017-02-28 09:51:27 +0100

    Render cards only on props change

--- Marvelist.Web/src/components/common/HomePage.js
+++ Marvelist.Web/src/components/common/HomePage.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Card1 from '../card/Card';
+import Card from '../card/Card';
 import { fetchHome, folllowComic } from '../../actions/seriesActions';
 import { PLACE } from '../../actions/constants';
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
-    this.hasData = false;
+    this.data = "";
   }
   componentWillMount() {
     if (this.props.user.loggedIn) {
@@ -16,16 +16,15 @@ class HomePage extends React.Component {
   }
   componentWillReceiveProps(nextProps) {
     if (this.props.home !== nextProps.home) {
-      this.hasData = true;
+      let {follow} = nextProps;
+      this.data = nextProps.home.map((b, i) => <Card key={i} follow={follow} data={b} link={PLACE.HOME} />);
     }
   }
 
   render() {
-    let {follow} = this.props;
-    const comics = this.hasData ? this.props.home.map((b, i) => <Card1 key={i} follow={follow} data={b} link={PLACE.HOME} loading={false} />) : "";
     return (
       <div className="row cards">
-        {comics}
+        {this.data}
       </div>
     );
   }
--- Marvelist.Web/src/components/series/SeriesPage.js
+++ Marvelist.Web/src/components/series/SeriesPage.js
@@ -4,22 +4,27 @@ import Card from '../card/Card';
 import TextHeader from './TextHeader';
 import { fetchSeries, folllowSeries, search } from '../../actions/seriesActions';
 import { PLACE } from '../../actions/constants';
+import { Switch, Icon } from 'antd';
 
 class Series extends React.Component {
   constructor(props) {
     super(props);
-    this.filterBoxChange = this.filterBoxChange.bind(this);
+    this.mapData = this.mapData.bind(this);
     this.state = { showFollowed: true };
+    this.data = "";
   }
 
   componentWillMount() {
     this.getData(this.props);
   }
 
-
   componentWillReceiveProps(nextProps) {
     if (this.props.params != nextProps.params) {
       this.getData(nextProps);
+    }
+    if (this.props.series !== nextProps.series) {
+      let {follow} = nextProps;
+      this.data = this.mapData(nextProps);
     }
   }
 
@@ -34,22 +39,22 @@ class Series extends React.Component {
   }
   filterBoxChange() {
     this.setState({ ...this.state, showFollowed: !this.state.showFollowed });
-
+    this.data = this.mapData(this.props);
   }
-  getFilteredSeries() {
-    return this.state.showFollowed ? this.props.series : this.props.series.filter(x => x.following == false);
+  mapData(props) {
+    const series = this.state.showFollowed ? props.series : props.series.filter(x => x.following == false);
+    return series.map((b, i) => <Card key={i} follow={props.follow} data={b} link={PLACE.SERIES} />);
   }
   render() {
-    let {follow} = this.props;
-    const seriesList = this.getFilteredSeries().map((b, i) => <Card key={i} follow={follow} data={b} link={PLACE.SERIES} />);
     return (
       <div className="row cards">
         <TextHeader text={this.props.params.text} />
+         <Switch checkedChildren={"Show following series"} unCheckedChildren={"Hide following series"} />
         <div className="paper-toggle">
           <input type="checkbox" id="toggle" name="toggle" onChange={this.filterBoxChange} checked={this.state.showFollowed} />
           <label htmlFor="toggle">Show following series</label>
         </div>
-        {seriesList}
+        {this.data}
       </div>
     );
   }
