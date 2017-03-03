4c9d8f3e74da30169dfac1d1959d40222873b199
gregorysl <gregorysl@gmail.com>
2017-03-03 09:36:00 +0100

    add loading in reducers for card changes indicator

--- Marvelist.Web/src/components/card/Card.js
+++ Marvelist.Web/src/components/card/Card.js
@@ -7,22 +7,17 @@ import { Spin, Row, Col, Card } from 'antd';
 class MarvelistCard extends React.Component {
     constructor(props) {
         super(props);
-        this.loading = false;
         this.handleClick = this.handleClick.bind(this);
     }
-    componentWillReceiveProps(nextProps) {
-        if (this.props.data !== nextProps.data) {
-            this.loading = false;
-        }
-    }
+
     handleClick() {
-        this.loading = true;
         this.props.follow(this.props.data.id, this.props.data.seriesId);
     }
+    
     render() {
         return (
             <Col className="series-card" xs={{ span: 8 }} sm={{ span: 6 }} lg={{ span: 4 }} >
-                <Spin size="large" spinning={this.loading}>
+                <Spin size="large" spinning={this.props.data.loading}>
                     <Card bordered={false} bodyStyle={{ padding: 0 }}>
                         <Cover link={this.props.link} {...this.props.data} />
                         {this.props.data.date && <CoverDate comicDate={this.props.data.date} />}
--- Marvelist.Web/src/reducers/seriesReducer.js
+++ Marvelist.Web/src/reducers/seriesReducer.js
@@ -1,20 +1,26 @@
-import { FETCH_SERIES_SUCCESS, FETCH_SERIES_BY_ID_SUCCESS, FETCH_HOME_COMICS_SUCCESS, SEARCH_SUCCESS, FOLLOW_SERIES_SUCCESS } from '../actions/constants';
+import * as consts from '../actions/constants';
 
 export const seriesReducer = (state = [], action) => {
     switch (action.type) {
-        case FETCH_SERIES_SUCCESS:
-            return action.series;
-        case SEARCH_SUCCESS:
-            return action.series;
-        case FOLLOW_SERIES_SUCCESS:
-            for (let i in state) {
-                let item = state[i];
-                if (item.id == action.id) {
-                    item.following = !item.following;
-                    break;
-                }
+        case consts.FETCH_SERIES_SUCCESS:
+            return action.series.map(x => ({ ...x, loading: false }));
+        case consts.SEARCH_SUCCESS:
+            return action.series.map(x => ({ ...x, loading: false }));
+        case consts.FOLLOW_SERIES: {
+            if (!action.detailPage) {
+                let idx = state.findIndex(x => x.id == action.id);
+                let item = state[idx];
+                item.loading = true;
             }
             return [...state];
+        }
+        case consts.FOLLOW_SERIES_SUCCESS: {
+            let idx = state.findIndex(x => x.id == action.id);
+            let item = state[idx];
+            item.following = !item.following;
+            item.loading = false;
+            return [...state];
+        }
         default:
             return state;
     }
@@ -22,8 +28,20 @@ export const seriesReducer = (state = [], action) => {
 const initialDetailState = { "comicCount": 0, "read": 0, "comics": [], "id": 0, "url": "", "title": "", "thumbnail": "", "description": null, "following": true };
 export const seriesDetailsReducer = (state = initialDetailState, action) => {
     switch (action.type) {
-        case FETCH_SERIES_BY_ID_SUCCESS:
+        case consts.FETCH_SERIES_BY_ID_SUCCESS: {
+            let comics = action.seriesDetails.comics.map(x => ({ ...x, loading: false }));
+            action.seriesDetails.comics = comics;
             return action.seriesDetails;
+
+        }
+        case consts.FOLLOW_COMIC: {
+            if (!action.home) {
+                let idx = state.comics.findIndex(x => x.id == action.id);
+                let item = state.comics[idx];
+                item.loading = true;
+            }
+            return { ...state };
+        }
         default:
             return state;
     }
@@ -31,8 +49,17 @@ export const seriesDetailsReducer = (state = initialDetailState, action) => {
 
 export const homeComicsReducer = (state = [], action) => {
     switch (action.type) {
-        case FETCH_HOME_COMICS_SUCCESS:
-            return action.homeComics;
+        case consts.FETCH_HOME_COMICS_SUCCESS:
+            return action.homeComics.map(x => ({ ...x, loading: false }));
+        case consts.FOLLOW_COMIC: {
+            if (action.home) {
+                let idx = state.findIndex(x => x.id == action.id);
+                let item = state[idx];
+                item.loading = true;
+            }
+            return [...state];
+        }
+
         default:
             return state;
     }
