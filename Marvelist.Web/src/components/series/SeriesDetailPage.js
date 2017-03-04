2c468ed90bb0d4745cd99cb9516579fed7c6b887
gregorysl <gregorysl@gmail.com>
2017-03-04 14:51:23 +0100

    Parse data on props change and use detail comp

--- Marvelist.Web/src/components/series/SeriesDetailPage.js
+++ Marvelist.Web/src/components/series/SeriesDetailPage.js
@@ -3,47 +3,28 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchSeriesById, folllowComic, folllowSeries, readAllComic } from '../../actions/seriesActions';
 import Card from '../card/Card';
-import ReadAllButton from './ReadAllButton';
-import FollowButton from './FollowButton';
-import ProgressBar from './ProgressBar';
-import { Row, Col } from 'antd';
+import DetailsHeader from './DetailsHeader';
 
 class SeriesDetails extends React.Component {
     constructor(props) {
         super(props);
+        this.data = "";
     }
     componentWillMount() {
         this.props.fetchById(this.props.params.id);
     }
     componentWillReceiveProps(nextProps) {
         if (this.props.seriesDetails !== nextProps.seriesDetails) {
-            this.hasComics = true;
+            this.data = nextProps.seriesDetails.comics.map((x, i) => <Card key={i} follow={nextProps.follow} data={x} />);
         }
     }
     render() {
-        let {follow, folllowSeries, readAllComic} = this.props;
-        const details = this.props.seriesDetails;
-        const comicList = details.comics.map((x, i) => <Card key={i} follow={follow} data={x} />);
-        let percent = Math.round((details.read / details.comicCount) * 100 * 100) / 100;
+        let { folllowSeries, readAllComic, seriesDetails } = this.props;
         return (
             <div className="row">
-                <Row type="flex" justify="center" style={{ 'paddingTop': '20px' }} >
-                    <Col md={5} sm={24} style={{ height: '340px' }} >
-                        <img style={{ height: '100%' }} src={details.thumbnail} alt="" />
-                    </Col>
-                    <Col md={3} sm={12} xs={24} >
-                        <ProgressBar percent={percent} following={details.following} />
-                    </Col>
-                    <Col md={8} sm={12} >
-                        <h2>{details.title}</h2>
-                        <FollowButton following={details.following} click={folllowSeries} id={details.id} />
-                        <ReadAllButton following={details.following} percent={percent} click={readAllComic} id={details.id} />
-                        <p>{details.details}</p>
-
-                    </Col>
-                </Row>
+                <DetailsHeader details={seriesDetails} folllowSeries={folllowSeries} readAllComic={readAllComic} />
                 <div className="row cards">
-                    {comicList}
+                    {this.data}
                 </div>
             </div>
         );
