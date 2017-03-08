f89f92ef311b90c385a4982c5f70c9fb38650f9f
gregorysl <gregorysl@gmail.com>
2017-03-08 09:22:59 +0100

    uptaded store to change with history

--- Marvelist.Web/src/reducers/index.js
+++ Marvelist.Web/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { seriesReducer, seriesDetailsReducer, homeComicsReducer } from './seriesReducer';
 import userReducer from './userReducer';
+import {routerReducer} from 'react-router-redux';
 
 export default combineReducers({
   series: seriesReducer,
   seriesDetails: seriesDetailsReducer,
   user: userReducer,
-  homeComics: homeComicsReducer
+  homeComics: homeComicsReducer,
+  routing: routerReducer
 });
--- Marvelist.Web/src/store/configureStore.js
+++ Marvelist.Web/src/store/configureStore.js
@@ -1,19 +1,52 @@
 import {createStore, compose, applyMiddleware} from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import allSagas from '../sagas';
 
-const configureStore = (initialState) => {
+
+function configureStoreProd(initialState) {
   const saga = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(saga)
+  const middlewares = [
+    saga,
+  ];
+
+  const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middlewares)
+    )
   );
   saga.run(function * () {
     yield allSagas;
   }); 
   return store;
-};
+}
+
+function configureStoreDev(initialState) {
+  const saga = createSagaMiddleware();
+  const middlewares = [
+    reduxImmutableStateInvariant(),
+    saga,
+  ];
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(...middlewares)
+    )
+  );
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+  
+  saga.run(function * () {
+    yield allSagas;
+  }); 
+  return store;
+}
+
+const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
 
 export default configureStore;
