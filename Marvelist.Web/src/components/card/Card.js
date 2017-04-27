ebc4f1577d6edbfa0cf14b27dfcb7c55747f037f
gregorysl <gregorysl@gmail.com>
2017-04-27 23:32:06 +0200

    Add progress bar for dashboard

--- Marvelist.Web/src/components/card/Card.js
+++ Marvelist.Web/src/components/card/Card.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import Cover from './Cover';
 import ActionsBar from './ActionsBar';
-import { Spin, Col, Card } from 'antd';
+import { Spin, Col, Card, Progress } from 'antd';
 
 class MarvelistCard extends React.Component {
     constructor(props) {
@@ -14,12 +14,15 @@ class MarvelistCard extends React.Component {
     }
 
     render() {
+        let percent = this.props.data.comicCount ? Math.round((this.props.data.read / this.props.data.comicCount) * 100 * 10) / 10 : null;
         return (
             <Col xs={12} sm={6} md={6} lg={4} >
                 <Spin size="large" spinning={this.props.data.loading}>
                     <Card bordered={false} bodyStyle={{ padding: 0 }}>
+                        {percent && <Progress percent={percent} />}
                         <Cover place={this.props.place} {...this.props.data} />
                         <ActionsBar click={this.handleClick} {...this.props.data} />
+
                     </Card>
                 </Spin>
             </Col>
