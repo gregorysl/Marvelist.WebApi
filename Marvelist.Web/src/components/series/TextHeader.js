f2938e14d2734bbf1db540e53ef4b36bdb665520
gregorysl <gregorysl@gmail.com>
2017-05-09 07:03:10 +0100

    Add raw text option for header

--- Marvelist.Web/src/components/series/TextHeader.js
+++ Marvelist.Web/src/components/series/TextHeader.js
@@ -2,13 +2,15 @@ import React, { PropTypes } from 'react';
 
 const TextHeader = (props) => {
     let text = !props.text ? "Series" : `Search results for "${props.text}"`;
+    const showRaw = !props.raw ? text : props.raw;
     return (
-        <h1>{text}</h1>
+        <h1>{showRaw}</h1>
     );
 };
 
 TextHeader.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    raw: PropTypes.string
 };
 
 export default TextHeader;
