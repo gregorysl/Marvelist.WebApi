df6b831a211778493ff673b0ca585200dd5f8e1d
gregorysl <gregorysl@gmail.com>
2017-05-11 22:30:10 +0200

    More TextHeader tests

--- Marvelist.Web/src/components/series/TextHeader.spec.js
+++ Marvelist.Web/src/components/series/TextHeader.spec.js
@@ -3,11 +3,9 @@ import { shallow } from 'enzyme';
 import TextHeader from './TextHeader';
 
 describe('<TextHeader />', () => {
-  it('should be an h1 element', () => {
-    const props = {
-    };
-    const wrapper = shallow(<TextHeader {...props} />);
-    expect(wrapper.type()).toEqual('h1');
+  it('should return h1 type', () => {
+    const wrapper = shallow(<TextHeader />);
+    expect(wrapper.type()).toEqual("h1");
   });
   it('should return text for search results with props text', () => {
     const props = {
@@ -16,9 +14,12 @@ describe('<TextHeader />', () => {
     const wrapper = shallow(<TextHeader {...props} />);
     expect(wrapper.find('h1').text()).toEqual("Search results for \"test\"");
   });
-  it('should return h1 without props', () => {
-    const wrapper = shallow(<TextHeader />);
-    expect(wrapper.type()).toEqual("h1");
+  it('should return raw text', () => {
+    const props = {
+      raw: "test"
+    };
+    const wrapper = shallow(<TextHeader {...props} />);
+    expect(wrapper.find('h1').text()).toEqual(props.raw);
   });
   it('should return text Series without props', () => {
     const wrapper = shallow(<TextHeader />);
