eff6f325cf32cadd30c7184961c6263d46d70da5
Grzegorz Slazak <grzegorz.slazak@fenergo.com>
2020-10-29 22:15:14 +0100

    migrate cover card

--- Marvelist.Web/src/components/card/Card.js
+++ Marvelist.Web/src/components/card/Card.js
@@ -2,15 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import Cover from "./Cover";
 import ActionsBar from "./ActionsBar";
-import { Spin, Col, Card, Progress } from "antd";
+import { Card, CardContent, Grid } from "@material-ui/core";
 
-class MarvelistCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
+const MarvelistCard = ({ toggleFollow, dashboard, follow, place, data }) => {
+  const handleClick = () => {
     const {
       props: { week, data, follow },
     } = this;
@@ -19,28 +14,26 @@ class MarvelistCard extends React.Component {
     } else {
       follow(data.id, data.seriesId);
     }
-  }
+  };
 
-  render() {
-    let percent =
-      this.props.data.comicCount !== 0
-        ? Math.round(
-            (this.props.data.read / this.props.data.comicCount) * 100 * 10
-          ) / 10
-        : 100;
-    return (
-      <Col xs={12} sm={6} md={6} lg={4} xl={2}>
-        <Spin size="large" spinning={this.props.data.loading}>
-          <Card bordered={false} bodyStyle={{ padding: 0 }}>
-            {this.props.dashboard && <Progress percent={percent} />}
-            <Cover place={this.props.place} {...this.props.data} />
-            <ActionsBar click={this.handleClick} {...this.props.data} />
-          </Card>
-        </Spin>
-      </Col>
-    );
-  }
-}
+  let percent =
+    data.comicCount !== 0
+      ? Math.round((data.read / data.comicCount) * 100 * 10) / 10
+      : 100;
+
+  // {/* Todo loader */}
+  return (
+    <Grid item xs={4}>
+      <Card>
+        <CardContent>
+          <Cover place={place} {...data} />
+          <ActionsBar click={() => handleClick()} {...data} />
+        </CardContent>
+        {/* {dashboard && <Progress percent={percent} />} */}
+      </Card>
+    </Grid>
+  );
+};
 
 MarvelistCard.propTypes = {
   toggleFollow: PropTypes.func,
--- Marvelist.Web/src/components/card/Cover.js
+++ Marvelist.Web/src/components/card/Cover.js
@@ -5,11 +5,12 @@ import CoverDate from "./CoverDate";
 import url from "../../images/not_found.png";
 
 const Cover = (props) => {
+  console.log(url);
   return (
     <div className="[ info-card ]">
       {props.date && <CoverDate comicDate={props.date} />}
-      <img alt="background" className="base" src={url} />
-      <img alt="cover" src={props.thumbnail} />
+      <img alt="background" className="basea" src={url} />
+      {/* <img alt="cover" src={props.thumbnail} /> */}
       <div className="[ info-card-details ] animate">
         <Title place={props.place} {...props} />
         <p className="[ info-card-detail ]">{props.description}</p>
