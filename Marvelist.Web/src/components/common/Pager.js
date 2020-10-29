28cdd25a8df00dcd50c13c9c4de33ca1cb0c7d02
Grzegorz Slazak <grzegorz.slazak@fenergo.com>
2020-10-29 20:13:02 +0100

    change to mui

--- Marvelist.Web/src/components/common/Pager.js
+++ Marvelist.Web/src/components/common/Pager.js
@@ -1,26 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Pagination } from "@material-ui/core";
 
 const Pager = ({ pageData, onChangePage }) => {
   let page = pageData.page + 1;
   const show = pageData.pageSize !== pageData.count;
-  return (
-    <div>
-      {show && (
-        <Pagination
-          count={pageData.count}
-          page={page}
-          onChange={onChangePage}
-        />
-      )}
-    </div>
+  return show ? (
+    <Pagination count={pageData.count} page={page} onChange={onChangePage} />
+  ) : (
+    <></>
   );
-};
-
-Pager.propTypes = {
-  pageData: PropTypes.object.isRequired,
-  onChangePage: PropTypes.func.isRequired,
 };
 
 export default Pager;
