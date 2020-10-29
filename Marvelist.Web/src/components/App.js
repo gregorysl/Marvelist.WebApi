16e7d07af3dbae157b05c31dcb772e84ac0c9e97
Grzegorz Slazak <grzegorz.slazak@fenergo.com>
2020-10-29 20:12:52 +0100

    Change to functional component

--- Marvelist.Web/src/components/App.js
+++ Marvelist.Web/src/components/App.js
@@ -12,32 +12,30 @@ import Dashboard from "../components/series/DashboardPage";
 import SeriesDetails from "../components/series/SeriesDetailPage";
 const { Header, Content } = Layout;
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Layout className="layout">
-          <Header>
-            <MyHeader />
-          </Header>
-          <Content style={{ padding: "0 50px" }}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-              <Route path="/series/:id" component={SeriesDetails} />
-              <Route path="/series" component={Series} />
-              <Route path="/search/:text" component={Series} />
-              <Route path="/week/:week?" component={Week} />
-              <Route path="/y:year" component={Series} />
-              <Route path="/dashboard" component={Dashboard} />
-            </Switch>
-          </Content>
-        </Layout>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout className="layout">
+        <Header>
+          <MyHeader />
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/series/:id" component={SeriesDetails} />
+            <Route path="/series" component={Series} />
+            <Route path="/search/:text" component={Series} />
+            <Route path="/week/:week?" component={Week} />
+            <Route path="/y:year" component={Series} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Content>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
